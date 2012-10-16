f1c473b0c052073731ffc705ff3a7059f6114cb6
Zhiqin Peng <zqpeng@scilearn.com.cn>
2012-10-16 15:00:25 +0800

    add Http interface files

---
+++ WeiDotaClient/Utils/HttpCom.h
@@ -0,0 +1,17 @@
+//
+//  HttpCom.h
+//  WeiDotaClient
+//
+//  Created by scilearn peng on 10/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HttpCom : NSObject
+{
+    
+}
+
++ (NSString*) sendHttpSycRequest:(NSString*)urlString;
+@end
